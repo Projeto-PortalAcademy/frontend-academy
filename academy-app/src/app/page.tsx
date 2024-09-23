c13c2d6318c1423058f81f0b1ed8b212bc3f61c5
Thiago <thiagotavares2002@gmail.com>
2024-09-23 17:34:12 -0300

    feat: ModeloBasePaginas

--- academy-app/src/app/page.tsx
+++ academy-app/src/app/page.tsx
@@ -1,9 +1,15 @@
+import Header from "@/components/Header/header";
 import Sidebar from "../components/Sidebar/sidebar";
 
 export default function Home() {
   return (
-    <div>
-      <Sidebar/>
+    <div className="flex h-screen">
+      <Sidebar />
+            {/* Content */}
+      <div className="flex-1">
+        <Header />
+          {/* Conteúdo aqui!!! */}
+      </div>
     </div>
   );
 }
--- academy-app/src/components/Sidebar/sidebar.tsx
+++ academy-app/src/components/Sidebar/sidebar.tsx
@@ -1,79 +1,61 @@
-import React from 'react';
+import React from "react";
+import Header from "../Header/header"; // Importe o Header
+import Image from "next/image";
+import IconDashboards from "../images/iconDashboards";
+import IconFrequencia from "../images/iconFrequencia";
+import IconAreas from "../images/iconAreas";
+import IconUsuarios from "../images/iconUsuarios";
+import IconLogo from "../images/iconLogo";
 
 function Sidebar() {
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
-      <div className="w-64" style={{ backgroundColor: 'rgba(12, 42, 88, 1)' }}>
+      <div className="w-64" style={{ backgroundColor: "rgba(12, 42, 88, 1)" }}>
         <div className="text-white flex flex-col">
           {/* Logo */}
           <div className="flex items-center justify-center py-4 border-b border-gray-700">
-            <img
-              src="/image/LOGO_2RP_acad.png" // Caminho do logo
-              alt="Logo"
-              className="h-16"
-            />
+          <IconLogo className="h-16 w-60" />
           </div>
-          
+
           {/* Menu items */}
           <nav className="mt-10">
             <a
               href="#"
               className="flex flex-col items-center py-2 px-6 text-white hover:bg-blue-700"
             >
-              <img
-                src="/image/Frequencia.svg" // Caminho da imagem Frequência
-                alt="Frequência"
-                className="h-6 w-6 mb-1" // Margem inferior para espaçamento
-              />
-              Frequência
+              <IconFrequencia className="h-16 w-60" />
+
+              
             </a>
 
             <a
               href="#"
               className="flex flex-col items-center py-2 px-6 text-white hover:bg-blue-700"
             >
-              <img
-                src="/image/dashboards.svg" // Caminho para imagem vazia
-                alt="Dashboards"
-                className="h-6 w-6 mb-1" // Margem inferior para espaçamento
-              />
-              Dashboards
+              <IconDashboards className="h-16 w-60" />
+
             </a>
 
             <a
               href="#"
               className="flex flex-col items-center py-2 px-6 text-white hover:bg-blue-700"
             >
-              <img
-                src="/image/areas.svg" // Caminho para imagem vazia
-                alt="Áreas"
-                className="h-6 w-6 mb-1" // Margem inferior para espaçamento
-              />
-              Áreas
+              <IconAreas className="h-16 w-60" />
+
             </a>
 
             <a
               href="#"
               className="flex flex-col items-center py-2 px-6 text-white hover:bg-blue-700"
             >
-              <img
-                src="/image/usuarios.svg" // Caminho para imagem vazia
-                alt="Usuários"
-                className="h-6 w-6 mb-1" // Margem inferior para espaçamento
-              />
-              Usuários
+              <IconUsuarios className="h-16 w-60" />
             </a>
           </nav>
         </div>
       </div>
 
-      {/* Content */}
-      <div className="flex-1 bg-gray-100 p-10">
-        {/* Aqui vai o conteúdo da página */}
-        <h1 className="text-3xl font-semibold">Dashboards</h1>
-        {/* Gráfico ou outro conteúdo */}
-      </div>
+
     </div>
   );
 }
