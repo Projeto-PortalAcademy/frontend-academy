35a8d35cda02c2d67025a3940ebcf1383d381593
Thiago <thiagotavares2002@gmail.com>
2024-10-10 20:45:44 -0300

    styles: adicionandoComponenteDeSino

--- academy-app/src/components/Header/header.tsx
+++ academy-app/src/components/Header/header.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { FaChevronDown, FaUserCircle } from 'react-icons/fa'; // Ícone de usuário do react-icons
+import { FaChevronDown, FaUserCircle, FaBell } from 'react-icons/fa'; // Ícones do react-icons
 
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center justify-end w-full py-4 px-8 border-gray-200">
+    <header className="flex items-center justify-end w-full py-4 px-8"> {/* Removi a classe 'border-b border-gray-200' */}
+      {/* Ícone de Sino */}
+      <div className="mr-4">
+        <FaBell className="text-gray-800" size={20} /> {/* Ícone de sino ajustado */}
+      </div>
+
       {/* Ícone de Avatar do Usuário */}
       <div className="mr-4">
         <FaUserCircle className="text-gray-800" size={30} /> {/* Ajuste o tamanho com 'size' */}
       </div>
-      
+
       {/* Nome do usuário */}
       <div className="text-gray-800 text-sm font-semibold">
         Vinicius Antunes
