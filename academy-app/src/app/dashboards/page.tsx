518e9ebe8ed25b506126b86c8572930a9b33ad6c
Felipe Camargo <felipe.moreira@2rpnet.com>
2024-10-10 21:48:44 -0300

    feat(academy-app/): Paginação base das telas

---
+++ academy-app/src/app/dashboards/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboards = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Dashboards</h1>
+      <p>Conteúdo da página de dashboards.</p>
+    </div>
+  );
+};
+
+export default Dashboards;
---
+++ academy-app/src/app/usuarios/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Usuarios = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Usuários</h1>
+      <p>Conteúdo da página de usuários.</p>
+    </div>
+  );
+};
+
+export default Usuarios;
