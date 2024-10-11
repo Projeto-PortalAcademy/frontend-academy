b19891219eddf01f3f62c80502e7818114eb9f5c
Thiago <thiagotavares2002@gmail.com>
2024-10-10 21:46:20 -0300

    wip: paginaDeGraficosNãoFinalizada

--- academy-app/src/app/page.tsx
+++ academy-app/src/app/page.tsx
@@ -1,10 +1,15 @@
+import FilterAndSearch from "@/components/FilterAndSearch/FilterAndSearch"
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
-            {/* Content */}
-      <div className="flex-1">
-          {/* Conteúdo aqui!!! */}
+    <div>
+      <div className="flex justify-center items-center ml-60">
+        {/* Content */}
+        <h1 className="text-3xl font-bold">Dashboards</h1>
+      </div>
+      {/* Filtro e Pesquisa posicionados conforme a imagem */}
+      <div className="flex items-center justify-start space-x-4 mt-20">
+        <FilterAndSearch />
       </div>
     </div>
   );
