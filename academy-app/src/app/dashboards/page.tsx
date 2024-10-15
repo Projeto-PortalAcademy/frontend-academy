48135db8e0062bf2e91351329b5ea1726bc8c1dc
camilayatabe <camila.yatabe@gmail.com>
2024-10-15 15:11:36 -0300

    feature: add component to dashboards page

--- academy-app/src/app/dashboards/page.tsx
+++ academy-app/src/app/dashboards/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Estagiario from "@/components/Estagiario/estagiario";
 
 const Dashboards = () => {
+  const estagiarioData = {
+    nome: "Amy Winehouse",
+    area: "Ciência de dados",
+    squad: "LSD",
+    skills: ["Comunicação", "Organização", "Liderança", "Gestão do Tempo", "Criatividade"],
+    tecnologias: ["Python", "Apache Spark", "Bitbucket", "Apache Beam", "Apache Airflow"],
+  };
+
   return (
     <div>
       <h1 className="text-2xl font-bold">Dashboards</h1>
+      <Estagiario {...estagiarioData} />
       <p>Conteúdo da página de dashboards.</p>
     </div>
   );
