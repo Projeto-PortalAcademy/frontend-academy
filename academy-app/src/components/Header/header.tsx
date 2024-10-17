a1f2d67749977d3c323a48d9e61f45602b687b68
Thiago <thiagotavares2002@gmail.com>
2024-10-16 21:05:54 -0300

    refactor: AjustandoComponentes

--- academy-app/src/components/Header/header.tsx
+++ academy-app/src/components/Header/header.tsx
@@ -4,26 +4,19 @@ import { FaChevronDown, FaUserCircle, FaBell } from "react-icons/fa"; // Ícones
 const Header: React.FC = () => {
   return (
     <header className="flex items-center justify-end w-full py-4 px-8">
-      {" "}
-      {/* Removi a classe 'border-b border-gray-200' */}
       {/* Ícone de Sino */}
       <div className="mr-4">
-        <FaBell className="text-gray-800" size={20} />{" "}
-        {/* Ícone de sino ajustado */}
+        <FaBell className="text-gray-800" size={20} /> {/* Ícone de sino ajustado */}
       </div>
-      {/* Ícone de Avatar do Usuário */}
-      <div className="mr-4">
-        <FaUserCircle className="text-gray-800" size={30} />{" "}
-        {/* Ajuste o tamanho com 'size' */}
+
+      {/* Contêiner para Avatar e Nome do Usuário */}
+      <div className="flex items-center space-x-2 mr-4">
+        <FaUserCircle className="text-gray-800" size={30} /> {/* Ícone de Avatar */}
+        <span className="text-gray-800 text-sm font-semibold">Vinicius Antunes</span>
       </div>
-      {/* Nome do usuário */}
-      <div className="text-gray-800 text-sm font-semibold">
-        Vinicius Antunes
-      </div>
+
       {/* Ícone de seta para baixo */}
       <div className="ml-2 mt-1">
-        {" "}
-        {/* Ajuste de margem */}
         <FaChevronDown className="text-gray-500" size={14} />
       </div>
     </header>
