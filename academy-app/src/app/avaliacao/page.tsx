8fffb31e7921641ddb0a2348185d90d657391cab
Felipe Camargo <felipe.moreira@2rpnet.com>
2024-11-22 09:17:08 -0300

    feat(academy-app): Implementação do questionario de avaliação

--- academy-app/src/app/avaliacao/page.tsx
+++ academy-app/src/app/avaliacao/page.tsx
@@ -1,11 +1,33 @@
-// pages/index.tsx
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import EvaluationHeader from "@/components/EvaluationHeader/EvaluationHeader";
 import FilterAndSearch from "@/components/FilterAndSearch/FilterAndSearch";
 import EvaluationCard from "@/components/EvaluationCard/EvaluationCard";
 import CreateEvaluationButton from "@/components/CreateEvaluationButton/CreateEvaluationButton";
 
+interface EvaluationData {
+  id: number;
+  avaliado: string;
+  avaliador: string;
+  data: string;
+}
+
 export default function Home() {
+  const [evaluations, setEvaluations] = useState<EvaluationData[]>([
+    { id: 1, avaliado: "Felipe Camargo", avaliador: "Rodney Rick", data: "05/11/2024" },
+    { id: 2, avaliado: "Erick Cassoli", avaliador: "Rodney Rick", data: "28/10/2024" },
+    { id: 3, avaliado: "Vinicius Lino", avaliador: "Thiago Kaijyama", data: "24/09/2024" },
+    { id: 4, avaliado: "Vinicius Lino", avaliador: "Thiago Kaijyama", data: "24/09/2024" },
+  ]);
+
+  // Função para adicionar uma nova avaliação
+  const addEvaluation = (newEvaluation: Omit<EvaluationData, "id">) => {
+    setEvaluations((prev) => [
+      ...prev,
+      { id: prev.length + 1, ...newEvaluation }, // Adiciona com um ID incremental
+    ]);
+  };
+
   return (
     <div className="p-8">
       <div className="flex justify-center items-center mb-8">
@@ -20,34 +42,20 @@ export default function Home() {
 
       {/* Lista de Cards de Avaliação */}
       <div>
-        <EvaluationCard
-          id={1}
-          avaliado="Vinicius Lino"
-          avaliador="Thiago Kaijyama"
-          data="24/09/2024"
-        />
-        <EvaluationCard
-          id={2}
-          avaliado="Vinicius Lino"
-          avaliador="Thiago Kaijyama"
-          data="24/09/2024"
-        />
-        <EvaluationCard
-          id={3}
-          avaliado="Vinicius Lino"
-          avaliador="Thiago Kaijyama"
-          data="24/09/2024"
-        />
-        <EvaluationCard
-          id={4}
-          avaliado="Vinicius Lino"
-          avaliador="Thiago Kaijyama"
-          data="24/09/2024"
-        />
+        {evaluations.map((evaluation) => (
+          <EvaluationCard
+            key={evaluation.id}
+            id={evaluation.id}
+            avaliado={evaluation.avaliado}
+            avaliador={evaluation.avaliador}
+            data={evaluation.data}
+            onClick={(id) => console.log("Card clicado com ID: ", id)}
+          />
+        ))}
       </div>
 
       {/* Botão de Criar Avaliação */}
-      <CreateEvaluationButton />
+      <CreateEvaluationButton onCreate={addEvaluation} />
     </div>
   );
 }
